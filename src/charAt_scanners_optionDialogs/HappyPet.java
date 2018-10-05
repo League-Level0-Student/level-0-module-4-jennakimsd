b27ffe77909ae4d2ce0f250ba3b67082ac8a5bbc
jennakimsd <league@WTS13.attlocal.net>
2018-10-05 16:53:05 -0700

    I did String Methods, Own Adventure, and Happy Pet

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -8,11 +8,11 @@ public class HappyPet {
 	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		
-	}
+	
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String pet= JOptionPane.showInputDialog("What kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-	
+	while(happinessLevel<50) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
@@ -20,28 +20,37 @@ String pet= JOptionPane.showInputDialog("What kind of pet do you want to buy?");
 					new String[] { "Give it food", "Walk it", "Buy a toy for it" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-		System.out.prinln(task);
+			if(task==0) {
+				giveFood();
+			}
+			if(task==1) {
+				walkPet();
+			}
+			if(task==2) {
+				buyToy();
+			}
+			
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
-	
+}
+	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 	static void giveFood() {
-		JOptionPane.showMessageDialog(null, "That's exactly what they needed");
-		happinessLevel= happinessLevel+5;
-		System.out.println(happinessLevel+5);
+		JOptionPane.showMessageDialog(null, "That's exactly what they needed, they were hungry");
+		happinessLevel= happinessLevel+10;
+		System.out.println(happinessLevel);
 	}
 	static void walkPet() {
 		JOptionPane.showMessageDialog(null, "They really needed to go to the bathroom");
-		happinessLevel= happinessLevel+15;
-		System.out.println(happinessLevel+15);
+		happinessLevel= happinessLevel+20;
+		System.out.println(happinessLevel);
 	}
 	static void buyToy() {
 		JOptionPane.showMessageDialog(null, "They don't really like the toy");
 		happinessLevel= happinessLevel-5;
-		System.out.println(happinessLevel-5);
+		System.out.println(happinessLevel);
 	}
 }
--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -14,13 +14,15 @@ public class StringMethods1 {
 String thing= JOptionPane.showInputDialog("Type something");
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(thing.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(thing.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(int i=0; i<thing.length(); i++) {
+System.out.println(thing.charAt(i));
+}
 
 		
 		// 5. Pick a char inside your String, and use a loop to determine
@@ -29,13 +31,13 @@ String thing= JOptionPane.showInputDialog("Type something");
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 
-		String character= JOptionPane.showInputDialog("Pick a character from your string");
-		System.out.println(character);
-		for(int i=0; i<thing.length(); i++) {
-			thing.length();
-		if(character.equals(thing i){
-			System.out.println(thing.charAt(i));
-		}
+String character= JOptionPane.showInputDialog("Pick a character from your string");
+System.out.println(character);
+for(int i=0; i<thing.length(); i++) {
+	if(thing.charAt(i)==character.charAt(0)) {
+		System.out.println(i);
+	}
+}
 	}
 }
 
